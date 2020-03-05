4cafad5ec11b2feac0433428401dcbca0eadeaa7
Rafael Grigoryan <grraph.bio@gmail.com>
2020-03-05 03:39:16 +0300

    updated scripts for 2018 experiments

--- analysis_and_plotting.py
+++ analysis_and_plotting.py
@@ -23,24 +23,32 @@ import iter_topography_fork
 import analysis_and_plotting_functions as aps
 
 # %%
-plt.rcParams['figure.figsize'] = [12,12]
+plt.rcParams['figure.figsize'] = [16,9]
 # %%
 # Load dataset into memory (if short of memory, use preload=False)
 ds = dataset.DatasetReader(data_path=folders.database_path, preload=True)
 #%%
+#delta plots for every user
 for user in set(ds.markup['user']):
-    subset = ds.markup.loc[ (ds.markup['user'] == user) &
-                            (ds.markup['reg'] == 'all_neutral')
+    userdict = {}
+    for reg in set(ds.markup['reg']):
+        subset = ds.markup.loc[ (ds.markup['user'] == user) &
+                                (ds.markup['reg'] == reg)
+                                ]
+    
+        userdict[reg] = aps.subset(ds, subset, drop_channels=['A2', 'Fp1'], reference = 'average')['delta']
+    p = aps.plot_evoked_response(userdict, title=f'user {user}')
+#%%
+#delta plots for grand average
+u1 = min(set(ds.markup['user']))
+u2 = max(set(ds.markup['user']))
+ga_dict = {}
+for reg in set(ds.markup['reg']):
+    subset = ds.markup.loc[(ds.markup['reg'] == reg)
                             ]
 
-    subset = aps.subset(ds, subset, drop_channels=['A1'])
-    aps.plot_evoked_response(subset)
-
-
-subset = ds.markup.loc[ ds.markup['reg'] == 'all_neutral'
-                        ]
-subset = aps.subset(ds, subset, drop_channels=['A1'], reference = 'average')
-p = aps.plot_evoked_response(subset)
+    ga_dict[reg] = aps.subset(ds, subset, drop_channels=['A2', 'Fp1'], reference = 'average')['delta']
+p = aps.plot_evoked_response(ga_dict, title=f'grand average, users {u1}-{u2}')
 
 
 # %%
