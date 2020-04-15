5219b5c8643bea74a66c058fa96c154a595478cf
Rafael Grigoryan <grraph.bio@gmail.com>
2020-04-15 18:01:59 +0300

    comments in constants.py

--- constants.py
+++ constants.py
@@ -1,22 +1,29 @@
 import mne
 import pathlib
 
-mne.set_log_level(verbose=0)
+mne.set_log_level(verbose=0) # global mne log level, is set on import
 
+# technical markers, present in envnt data
 StartCycle = 777
 EndCycle = 888
 EndLearn = 888999
 StartPlay = 999888
 EndAll = 999
+technical_markers = [StartCycle, EndCycle, EndLearn, EndAll, StartPlay]
+
+# ECG analysis parameeters
 Rpeak_event = 200
 rejected_Rpeak_event = 404
 Rpeaks_filename ='Rpeaks.npy'
-technical_markers = [StartCycle, EndCycle, EndLearn, EndAll, StartPlay]
 
+# Folder parameters 
 parents = list(pathlib.Path(__file__).parents)
 pics_folder = pathlib.Path(parents[0] / 'pics')
+
+# True for row-column or otherwise grouped BCI spellers
 BCI_type_gropued = False
 
+# EEG channel settings
 ch_names = ['Fp1', 'ecg', 'Fp2', 'F3', 'Fz', 'F4', 'FC5', 'FC3',
             'FC1', 'FCz', 'FC2', 'FC4', 'FC6', 'T7', 'C5', 'C3',
             'C1', 'Cz', 'C2', 'C4', 'C6', 'T8', 'CP5', 'A1',
@@ -32,25 +39,32 @@ eog_channels = 'Fp1,Fp2' # need to be string like "ch1,ch2" due to
                          #	-> sumbit bug?
                          # None if using dedicated EOG channels
 montage = 'standard_1005'
+
+# Sampling frequency
 fs = 500
 ms_factor = 1000 // fs
 
-events_offset = 0.1
+events_offset = 0.1 # time lag between synchronised event and actual stimuli activation
+                    # Needs to be measured for every experiment.
 
+# Epochs timing settings
 epochs_tmin = -0.1
 epochs_tmax = 0.8
 
+# baseline settings
 epochs_baseline = None
 evoked_baseline = (-0.05, 0)
 test_baseline = (0,0)
 
+# Preprocessing settings: filtering
 butter_filt = [0.1, 35]
 butter_order = 4
 notch = 50
 
-n_ica_components = None
-mne_verbose_level = 1
+# Preprocessing settings: ICA
+n_ica_components = None # if None, uses maximum number of components
 
+# plotting settings
 uV_scaler = 1e6
 legend_loc = (-5, 0)
 plot_colors = {'target': 'red',
